c2d9fd92a9d49256e4b46cd8f0a8bd6b1d4d4265
Mirrayy <miraymutlu_@hotmail.com>
2023-11-12 17:28:41 +0300

    1.4 testleri tamamlandı

--- 210401039/Labex1_Q1_210401039_miray_mutlu.R
+++ 210401039/Labex1_Q1_210401039_miray_mutlu.R
@@ -2,7 +2,28 @@
 url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
 download.file(url, destfile = "MapsThatChangedOurWorld_StoryMap_Data.csv", mode = "wb")
 
+
+
+
+
+
+
 # Dosyayı Okuma ve Data Frame Oluşturma
 df <- read.csv("./MapsThatChangedOurWorld_StoryMap_Data.csv", sep = ";")
 
 maps <- data.frame(df)
+
+
+
+
+
+
+
+# Latitude sütunundaki "N" harflerini silme
+maps$Latitude <- gsub("N", "", maps$Latitude)
+
+
+
+
+# Longitude sütunundaki "W" harfi içeren satırların indekslerini bulma 1.4
+idx <- grep("W", maps$Longitude)
--- 210401039/Labex1_Q1_tests_210401039_miray_mutlu.R
+++ 210401039/Labex1_Q1_tests_210401039_miray_mutlu.R
@@ -16,12 +16,18 @@ print(current_dir)
 relative_path <- file.path(current_dir,"Labex1_Q1_210401039_miray_mutlu.R")
 print(relative_path)
 
+
+
+
 source(relative_path)
   test_that("Test : MapsThatChangedOurWorld_StoryMap_Data.csv adlı dosya belirtilen dizinde mevcuttur.", {
   file_path <- file.path(current_dir,"MapsThatChangedOurWorld_StoryMap_Data.csv")
   expect_true(file.exists(file_path), info = "Dosya mevcut değil.")
 })
 
+  
+  
+  
   test_that("Test : maps adlı değişken Global Workspace’de mevcuttur.", {
     expect_true(exists("maps", envir = .GlobalEnv), info = "maps adlı değişken mevcut değil.")
   })
@@ -29,9 +35,20 @@ source(relative_path)
   
   
   
+  
+  
   test_that("Test : maps nesnesi bir data.frame'dir.", {
     expect_is(maps, "data.frame", info = "maps nesnesi bir data.frame değil.")
   })
+  
+  
+  
+  test_that("Test : maps adlı data.frame'in ilk sütunu 'City' olmalıdır.", {
+  expect_equal(colnames(maps)[1], "City", info = "İlk sütun 'City' değil.")
+})
+  
+  
+  
   
   
   
